213d19524b0768e083fa51dcf7663563f5ce2d9b
somnath111993 <somnath.jan1@gmail.com>
2017-03-17 17:11:52 +0530

    Refactoring

--- client/components/actions/userManagement/UserList.jsx
+++ client/components/actions/userManagement/UserList.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import Request from 'superagent';
-import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
-  from 'material-ui/Table';
+import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
+// import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
+//   from 'material-ui/Table';
  import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
+
+import IconButton from 'material-ui/IconButton';
+// import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+// import IconMenu from 'material-ui/IconMenu';
+// import MenuItem from 'material-ui/MenuItem';
+import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
+ // import React from 'react';
+// import MobileTearSheet from '../../../MobileTearSheet';
+import Avatar from 'material-ui/Avatar';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+// import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
+import EditIcon from 'material-ui/svg-icons/editor/mode-edit';
+import DeleteIcon from 'material-ui/svg-icons/navigation/cancel';
 
 
 
 // let allUsers = [];
+const iconButtonElement = (
+	<div style={{alignItems: 'center'}}>
+		<IconButton tooltip="edit" tooltipPosition="bottom-left">
+	    <EditIcon color={grey400} />
+	  </IconButton>
+	  <IconButton tooltip="delete" tooltipPosition="bottom-left">
+			<DeleteIcon color={grey400} />
+	  </IconButton>
+	</div>
+  
+  
+);
+
+// const actionButtons = (
+//   <Grid>
+//   	<Row between="md">
+//   		<Col xs={6} md={6} >  			
+//     		<EditIcon />
+//   		</Col>
+//     	<Col xs={6} md={6} >
+//     		<DeleteIcon />
+//     	</Col>
+//     </Row>
+    
+//   </Grid>
+// );
 
 export default class UserList extends React.Component {
 	constructor(props) {
@@ -36,50 +78,21 @@ export default class UserList extends React.Component {
 	}
 	render() {
 		return (
-			<div>
-				<Card>
+			<div style={{display: 'flex', alignItems: 'center'}}>
+				<Card style={{width:'70%', margin:'auto' }}>
 					<CardText>
-						<Table
-		          height='300px'
-		          fixedHeader={true}
-		          fixedFooter={true}
-		          selectable={true}
-		          multiSelectable={true}
-		        >
-		          <TableHeader
-		            displaySelectAll={true}
-		            adjustForCheckbox={true}
-		            enableSelectAll={true}
-		          >
-		            <TableRow>
-		              <TableHeaderColumn colSpan="3" tooltip="Super Header" style={{textAlign: 'center'}}>
-		                Available Users
-		              </TableHeaderColumn>
-		            </TableRow>
-		            <TableRow>
-		              <TableHeaderColumn tooltip="Username">Username</TableHeaderColumn>
-		              <TableHeaderColumn tooltip="Name">Name</TableHeaderColumn>
-		              <TableHeaderColumn tooltip="Role">Role</TableHeaderColumn>
-		              <TableHeaderColumn tooltip="Email">Email</TableHeaderColumn>
-		            </TableRow>
-		          </TableHeader>
-		          <TableBody
-		            displayRowCheckbox={true}
-		            deselectOnClickaway={true}
-		            showRowHover={true}
-		            stripedRows={true}
-		          >
-		            {this.state.users.map( (user, index) => (
-		              <TableRow key={index} selected={user.selected}>
-		                <TableRowColumn>{user.username}</TableRowColumn>
-		                <TableRowColumn>{user.name}</TableRowColumn>
-		                <TableRowColumn>{user.role}</TableRowColumn>
-		                <TableRowColumn>{user.email}</TableRowColumn>		                
-		              </TableRow>
-		              ))}
-		          </TableBody>
-		          
-		        </Table>
+						<List>
+				      <Subheader>Available Users</Subheader>
+				      {this.state.users.map( (user, index) => (
+					      	<ListItem
+					      	key={index}		
+					      	disableKeyboardFocus={true}			      	
+					        primaryText={user.name}
+					        leftAvatar={<Avatar src="../../../assets/images/avt-default.jpg" />}
+					        rightIcon={iconButtonElement}					        
+					      	/>		              
+		          ))}				      
+				    </List>						
 					</CardText>
 				</Card>	
 				
