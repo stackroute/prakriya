75ce74a653d34e9b75fcbe8823f7c7325b0c74ee
somnath111993 <somnath.jan1@gmail.com>
2017-03-20 15:34:51 +0530

    Refactoring

--- client/components/actions/userManagement/UserList.jsx
+++ client/components/actions/userManagement/UserList.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import Request from 'superagent';
-import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
-import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
+// import React from 'react';
+// import Request from 'superagent';
+// import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
+// import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
 import IconButton from 'material-ui/IconButton';
 import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors'; 
+// import Avatar from 'material-ui/Avatar';
+// import {List, ListItem} from 'material-ui/List';
+// import Subheader from 'material-ui/Subheader';
+import React from 'react';
+import Request from 'superagent';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Chip from 'material-ui/Chip';
 import Avatar from 'material-ui/Avatar';
-import {List, ListItem} from 'material-ui/List';
-import Subheader from 'material-ui/Subheader';
+import Paper from 'material-ui/Paper';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 import Divider from 'material-ui/Divider';
 import EditIcon from 'material-ui/svg-icons/editor/mode-edit';
 import DeleteIcon from 'material-ui/svg-icons/navigation/cancel';
+import FlatButton from 'material-ui/FlatButton';
 
 
 
@@ -44,73 +52,64 @@ import DeleteIcon from 'material-ui/svg-icons/navigation/cancel';
 //   </Grid>
 // );
 
+
 export default class UserList extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			users: []
-		}
+		// this.state = {
+		// 	users: []
+		// }
 	}
-	componentDidMount() {
-		let th = this;
-		Request
-			.get('/admin/users')
-			.set({'Authorization': localStorage.getItem('token')})			
-			.end(function(err, res){
-		    if(err)
-		    	console.log(err);
-		    else
-		    	th.setState({
-		    		users: res.body
-		    	}) 
-		    	console.log("Users");
-		    	console.log(th.state.users);
+	// componentDidMount() {
+	// 	let th = this;
+	// 	Request
+	// 		.get('/admin/users')
+	// 		.set({'Authorization': localStorage.getItem('token')})			
+	// 		.end(function(err, res){
+	// 	    if(err)
+	// 	    	console.log(err);
+	// 	    else
+	// 	    	th.setState({
+	// 	    		users: res.body
+	// 	    	}) 
+	// 	    	console.log("Users");
+	// 	    	console.log(th.state.users);
 			    
 		    	
-		    // th.context.router.push('/app')
-		  });
-	}
+	// 	    // th.context.router.push('/app')
+	// 	  });
+	// }
 	render() {
+		console.log(this.props)
 		return (
 			<div>
-				<Card style={{width:'80%', margin:'auto' }}>
-					<Grid>
-						<Row middle="md">
-							<Subheader>Available Users</Subheader>
-						</Row>
-						
-							
-								<List>
-						      
-						      {this.state.users.map( (user, index) => (
-						      	<Row between="md" key={index}>
-						      		<Col md={6}>
-								      	<ListItem 
-								      	key={index}								      				      	
-								        primaryText={user.name}
-								        leftAvatar={<Avatar src="../../../assets/images/avt-default.jpg" />}								        				        
-								      	/>
-								      </Col>	
-								      <Col md={2} mdOffset={4}>	
-								      	<IconButton tooltip="delete" tooltipPosition="bottom-left">
-													<DeleteIcon color={grey400} />
-											  </IconButton>	
-							      	</Col>
-							      </Row>			              
-				          ))}				      
-						    </List>
-							
-							
-						
-							
-														
-							
-						
-					</Grid>
-					
-				</Card>	
 				
+					<Card>
+
+						<CardMedia overlay={<CardTitle title={this.props.currUser.name} subtitle={this.props.currUser.email} />}>
+				      <img src="../../../assets/images/avt-default.jpg" />
+				    </CardMedia>
+				    <CardTitle subtitle={this.props.currUser.username} />							
+						<CardActions>
+						  <FlatButton label="Edit" />
+						  <FlatButton label="Remove" />
+						</CardActions>
+
+				  </Card>
+			  
 			</div>
+			
 		);
 	}
 }
+
+
+
+
+// <IconButton tooltip="edit" tooltipPosition="bottom-left">
+								 //    <EditIcon color={grey400} />
+								 //  </IconButton>
+								  
+									// <IconButton tooltip="delete" tooltipPosition="bottom-left">
+									// 	<DeleteIcon color={grey400} />
+									// </IconButton>
--- client/components/actions/userManagement/Users.jsx
+++ client/components/actions/userManagement/Users.jsx
@@ -1,24 +1,83 @@
 import React from 'react';
+import Request from 'superagent';
+import {Grid, Row, Col} from 'react-flexbox-grid';
+import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
 import AddUser from './AddUser.jsx';
 import UserList from './UserList.jsx';
 
-const style = {
-	textAlign: 'center'
+const styles = {
+	heading: {
+		textAlign: 'center'
+	},	
+	card: {
+		margin: '10px 10px',
+		height: 'auto'
+	}
 }
+
+const style = {
+  height: 100,
+  width: 100,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
+
 
 export default class Users extends React.Component {
 	constructor(props) {
 		super(props)
-		// this.state = {
-
-		// }
+		this.state = {
+			users: []
+		}
+	}
+	componentDidMount() {
+		let th = this;
+		Request
+			.get('/admin/users')
+			.set({'Authorization': localStorage.getItem('token')})			
+			.end(function(err, res){
+		    if(err)
+		    	console.log(err);
+		    else
+		    	th.setState({
+		    		users: res.body
+		    	}) 
+		    	console.log("Users");
+		    	console.log(th.state.users);
+			    
+		    	
+		    // th.context.router.push('/app')
+		  });
 	}
 	render() {
 		return (
-			<div style={style} >
+			<div >
 				<AddUser />
-				<h1>User Management</h1>
-				<UserList />
+				<h1 style={styles.heading}>User Management</h1>
+				<Grid>
+					<Row>
+					<Col  md={4}>
+						<Paper style={style} zDepth={1}>
+							<FlatButton label="ADD" />
+						</Paper>
+					</Col>
+					{
+						this.state.users.map(function (user, index) {
+							return(
+								<Col md={4} key={index}>
+									 
+										<UserList style={styles.card} currUser={user} />
+									
+								</Col>
+								
+							)
+						})
+					}
+					</Row>
+				</Grid>
 			</div>
 		);
 	}
