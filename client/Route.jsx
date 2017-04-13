70bc408a3572d9f7061bbf4c9d7b8d2b1713c771
gajendra93 <gajendrasp.rj.9693@gmail.com>
2017-04-13 17:23:49 +0530

    Refactored

--- client/Route.jsx
+++ client/Route.jsx
@@ -5,10 +5,10 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import Request from 'superagent';
 import App from './views/App.jsx';
 import Welcome from './views/Welcome.jsx';
 import Login from './components/login/index.jsx';
-import SignUp from './components/signup/index.jsx';
 import Dashboard from './components/dashboard/index.jsx';
 import Roles from './components/roleManagement/index.jsx';
 import Users from './components/userManagement/index.jsx';
@@ -25,18 +25,40 @@ injectTapEventPlugin();
 
 const muiTheme = getMuiTheme(baseTheme);
 
-function requireAuth (nextState, replace, callback) {
+var user;
+
+let requireAuth = function(nextState, replace, callback) {
   const token = localStorage.getItem('token')
   if (!token) 
   	replace('/')
   return callback()
 }
 
-function alreadyLoggedIn (nextState, replace, callback) {
+let alreadyLoggedIn = function(nextState, replace, callback) {
   const token = localStorage.getItem('token')
-  if(token)
+  console.log(token);
+  if(token) {
   	replace('/app')
+  	// Request
+  	// 	.get('/dashboard/user')
+  	// 	.set({'Authorization': token})
+			// .end(function(err, res){
+			// 	if(err)
+			// 		console.log(err) 
+			// 	else {
+			// 		user = res.body
+			// 		replace('/app')
+			// 	}
+			// })
+  }
   return callback()
+}
+let rolesAccess = function (nextState, replace, callback) {
+	if(user) {
+		if(user.actions.indexOf('Roles') < 0)
+			replace('/app')
+	}
+	return callback()
 }
 
 ReactDOM.render(
@@ -46,7 +68,7 @@ ReactDOM.render(
 			<Route path="/login" component={Login} onEnter={alreadyLoggedIn} />
 			<Route path="/app" component={App} onEnter={requireAuth} >
 				<IndexRoute component={Dashboard} />
-				<Route path="/roles" component={Roles} />
+				<Route path="/roles" component={Roles} onEnter={rolesAccess} />
 				<Route path="/users" component={Users} />
 				<Route path="/candidates" component={Candidates} />
 				<Route path="/bulkupload" component={BulkUpload} />
