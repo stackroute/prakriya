69edc3f9e81b5d75f35fb3f75cd8314317789172
Ramvignesh Pasupathy <ramvignesh06@gmail.com>
2017-06-01 14:53:42 +0530

    Validations added for program flow dialogs

--- client/components/programFlow/Session.jsx
+++ client/components/programFlow/Session.jsx
@@ -6,11 +6,35 @@ import DatePicker from 'material-ui/DatePicker'
 import IconButton from 'material-ui/IconButton'
 import SaveIcon from 'material-ui/svg-icons/content/save'
 import {lightBlack} from 'material-ui/styles/colors'
-import Dialog from 'material-ui/Dialog';
-import RaisedButton from 'material-ui/RaisedButton';
-import DeleteIcon from 'material-ui/svg-icons/action/delete';
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import DeleteIcon from 'material-ui/svg-icons/action/delete'
 
 const styles = {
+	dialog: {
+		backgroundColor: '#DDDBF1',
+		borderBottom: '3px solid teal',
+		borderRight: '10px solid teal',
+		borderLeft: '10px solid teal'
+	},
+	dialogTitle: {
+		fontWeight: 'bold',
+		backgroundColor: 'teal',
+		color: '#DDDBF1',
+		textAlign: 'center'
+	},
+	actionsContainer: {
+		backgroundColor: 'teal',
+		borderTop: '0px',
+		marginTop: '0px'
+	},
+	actionButton: {
+		backgroundColor: '#DDDBF1',
+		width: '50%',
+		color: 'teal',
+		border: '1px solid teal',
+		height: '100%'
+	},
 	card: {
 		box: {
 				border: '0.5px solid #009ab1'
@@ -62,6 +86,11 @@ export default class Session extends React.Component {
 				Remarks: '',
 				showDialog: false
 			},
+			WeekErrorText: '',
+			ActivitiesText: '',
+			StatusErrorText: '',
+			ContextSetSessionErrorText: '',
+			SessionByErrorText: '',
 			saveDisabled: true
 		}
 
@@ -76,21 +105,21 @@ export default class Session extends React.Component {
 		this.updateSession = this.updateSession.bind(this)
 		this.enableSave = this.enableSave.bind(this)
 		this.handleClose = this.handleClose.bind(this)
-		this.deleteSession = this.deleteSession.bind(this);
+		this.deleteSession = this.deleteSession.bind(this)
+		this.validationSuccess = this.validationSuccess.bind(this)
+		this.resetFields = this.resetFields.bind(this)
 	}
 
 	componentWillMount() {
-		if(this.props.openDialog)
-		{
+		if(this.props.openDialog) {
 			this.setState({
 				showDialog: this.props.openDialog
 			})
 		}
-		else
-		{
+		else {
 			this.setState({
 				session: this.props.session,
-			})	
+			})
 		}
 	}
 
@@ -98,7 +127,8 @@ export default class Session extends React.Component {
 		let session = this.state.session
 		session.Activities = event.target.value
 		this.setState({
-			session: session
+			session: session,
+			ActivitiesErrorText: ''
 		})
 	}
 
@@ -106,7 +136,8 @@ export default class Session extends React.Component {
 		let session = this.state.session
 		session.Status = event.target.value
 		this.setState({
-			session: session
+			session: session,
+			StatusErrorText: ''
 		})
 	}
 
@@ -114,7 +145,8 @@ export default class Session extends React.Component {
 		let session = this.state.session
 		session.ContextSetSession = event.target.value
 		this.setState({
-			session: session
+			session: session,
+			ContextSetSessionErrorText: ''
 		})
 	}
 
@@ -122,7 +154,8 @@ export default class Session extends React.Component {
 		let session = this.state.session
 		session.SessionBy = event.target.value
 		this.setState({
-			session: session
+			session: session,
+			SessionByErrorText: ''
 		})
 	}
 
@@ -146,30 +179,31 @@ export default class Session extends React.Component {
 		let session = this.state.session
 		session.Week = event.target.value
 		this.setState({
-			session: session
+			session: session,
+			WeekErrorText: ''
 		})
 	}
 
 	addNewSession() {
 		let th = this
-		console.log('WaveID: ', th.props.waveID)
-		console.log('Add New Session')
-		let session = this.state.session
-		Request
-			.post('/mentor/addnewsession')
-			.set({'Authorization': localStorage.getItem('token')})
-			.send({session: session, waveID: th.props.waveID})
-			.end(function(err, res) {
-				if(err)
-		    	console.log(err)
-		    else{
-		    	console.log('Created session and Server responded', res.body)
-					th.setState({
-						saveDisabled: true
-					})
-					th.props.onSessionAddition()
-				}
-			})
+		if(this.validationSuccess()) {
+			let session = this.state.session
+			Request
+				.post('/mentor/addnewsession')
+				.set({'Authorization': localStorage.getItem('token')})
+				.send({session: session, waveID: th.props.waveID})
+				.end(function(err, res) {
+					if(err)
+			    	console.log(err)
+			    else{
+			    	console.log('Created session and Server responded', res.body)
+						th.setState({
+							saveDisabled: true
+						})
+						th.props.onSessionAddition()
+					}
+				})
+		}
 	}
 
 	updateSession() {
@@ -213,6 +247,52 @@ export default class Session extends React.Component {
 			})
 	}
 
+	validationSuccess() {
+		if(this.state.session.Week.trim().length == 0) {
+			this.setState({
+				WeekErrorText: 'This field cannot be empty'
+			})
+		} else if(this.state.session.Status.trim().length == 0) {
+			this.setState({
+				StatusErrorText: 'This field cannot be empty'
+			})
+		} else if(this.state.session.Activities.trim().length == 0) {
+			this.setState({
+				ActivitiesErrorText: 'This field cannot be empty'
+			})
+		} else if(this.state.session.SessionBy.trim().length == 0) {
+			this.setState({
+				SessionByErrorText: 'This field cannot be empty'
+			})
+		} else {
+			return true
+		}
+		return false
+	}
+
+	resetFields() {
+			this.state({
+				session: {
+					SessionID: '',
+					CourseName: '',
+					Week: '',
+					Activities: '',
+					Status: '',
+					ContextSetSession: '',
+					SessionBy: '',
+					SessionOn: new Date(),
+					Remarks: '',
+					showDialog: false
+				},
+				WeekErrorText: '',
+				ActivitiesText: '',
+				StatusErrorText: '',
+				ContextSetSessionErrorText: '',
+				SessionByErrorText: '',
+				saveDisabled: true
+			})
+	}
+
 	enableSave() {
 		this.setState({
 			saveDisabled: false
@@ -227,74 +307,105 @@ export default class Session extends React.Component {
 	}
 
 	render() {
-		let th = this;
-		if(this.props.openDialog)
-		{
+		let th = this
+		let title, actions
+		if(this.props.openDialog) {
+			title="ADD NEW SESSION"
+			actions = [
+				<FlatButton
+					label="Cancel"
+					style={styles.actionButton}
+					onTouchTap={this.handleClose}
+				/>,
+				<FlatButton
+    	 		label="Add Session"
+    	   	style={styles.actionButton}
+    			onClick={th.state.session.SessionID === '' ? th.addNewSession : th.updateSession}
+				/>
+			]
+		}
+		if(this.props.openDialog) {
 			return(
 			<Dialog
-			    	style={styles.dialog}
-	          title="Add New Session"
+			    	bodyStyle={styles.dialog}
+	          title={title}
+						titleStyle={styles.dialogTitle}
 	          open={this.state.showDialog}
 	          autoScrollBodyContent={true}
 	          onRequestClose={this.handleClose}
+						actionsContainerStyle={styles.actionsContainer}
+						actions={actions}
       >
-      <TextField
-							floatingLabelText='Week Number'
-							value={th.state.session.Week}
-							onChange={th.handleWeek}
-							onBlur={th.enableSave}
-			/><br/>
-			<TextField
-							floatingLabelText='Activities'
-							value={th.state.session.Activities}
-							onChange={th.handleActivities}
-							onBlur={th.enableSave}
-			/><br/>
-			<TextField
-							floatingLabelText='Status'
-							value={th.state.session.Status}
-							onChange={th.handleStatus}
-							onBlur={th.enableSave}
-			/><br/>
-			<TextField
-							floatingLabelText='ContextSetSession'
-							value={th.state.session.ContextSetSession}
-							onChange={th.handleContextSetSession}
-							onBlur={th.enableSave}
-				/><br/>
+			<div>
+				<div style={{width: '50%', border: '2px solid white', boxSizing: 'border-box', padding: '5px', display: 'inline-block'}}>
+	      <TextField
+								floatingLabelText='Week Number'
+								value={th.state.session.Week}
+								onChange={th.handleWeek}
+								onBlur={th.enableSave}
+								errorText={this.state.WeekErrorText}
+				/></div>
+				<div style={{width: '50%', border: '2px solid white', boxSizing: 'border-box', padding: '5px', display: 'inline-block'}}>
 				<TextField
-							floatingLabelText='SessionBy'
-							value={th.state.session.SessionBy}
-							onChange={th.handleSessionBy}
-							onBlur={th.enableSave}
-				/><br/>
-				<DatePicker
-							floatingLabelText='SessionOn'
-							defaultDate={new Date()}
-							onChange={th.handleSessionOn}
-							onBlur={th.enableSave}
-				/><br/>
+								floatingLabelText='Status'
+								value={th.state.session.Status}
+								onChange={th.handleStatus}
+								onBlur={th.enableSave}
+								errorText={this.state.StatusErrorText}
+				/></div>
+			</div>
+			<div>
+				<div style={{width: '100%', border: '2px solid white', boxSizing: 'border-box', padding: '5px', display: 'inline-block'}}>
 				<TextField
-							floatingLabelText='Remarks'
-							value={th.state.session.Remarks}
-							onChange={th.handleRemarks}
-							onBlur={th.enableSave}
-				/><br/>
-				<RaisedButton
-    	 		label="Add Session"
-    	   	primary={true}
-    			onClick={th.state.session.SessionID === '' ? th.addNewSession : th.updateSession}
+								floatingLabelText='Activities'
+								value={th.state.session.Activities}
+								onChange={th.handleActivities}
+								onBlur={th.enableSave}
+								errorText={this.state.ActivitiesErrorText}
 				/>
-				&emsp;
-  			<RaisedButton
-    	 		label="Cancel"
-    	   	primary={true}
-    			onTouchTap={this.handleClose}
-		    	 	/>
-				</Dialog>
+				</div>
+			</div>
+			<div>
+				<div style={{width: '33%', border: '2px solid white', boxSizing: 'border-box', padding: '5px', display: 'inline-block'}}>
+					<TextField
+									floatingLabelText='ContextSetSession'
+									value={th.state.session.ContextSetSession}
+									onChange={th.handleContextSetSession}
+									onBlur={th.enableSave}
+									errorText={this.state.ContextSetSessionErrorText}
+						/>
+				</div>
+				<div style={{width: '34%', border: '2px solid white', boxSizing: 'border-box', padding: '5px', display: 'inline-block'}}>
+					<TextField
+								floatingLabelText='SessionBy'
+								value={th.state.session.SessionBy}
+								onChange={th.handleSessionBy}
+								onBlur={th.enableSave}
+								errorText={this.state.SetSessionByErrorText}
+					/>
+				</div>
+				<div style={{width: '33%', border: '2px solid white', boxSizing: 'border-box', padding: '5px', display: 'inline-block'}}>
+					<DatePicker
+								floatingLabelText='SessionOn'
+								defaultDate={new Date()}
+								onChange={th.handleSessionOn}
+								onBlur={th.enableSave}
+					/>
+				</div>
+			</div>
+			<div>
+				<div style={{width: '100%', border: '2px solid white', boxSizing: 'border-box', padding: '5px', display: 'inline-block'}}>
+					<TextField
+								floatingLabelText='Remarks'
+								value={th.state.session.Remarks}
+								onChange={th.handleRemarks}
+								onBlur={th.enableSave}
+					/>
+				</div>
+			</div>
+			</Dialog>
 		)}
-		else
-		{ 
+		else {
 			let z = new Date(th.state.session.SessionOn);
 			let date = new Date(z.getFullYear()+'-'+(z.getMonth()+1)+'-'+z.getDate())
 			return (
@@ -359,4 +470,4 @@ export default class Session extends React.Component {
 			)
 		}
 	}
-}			
+}
