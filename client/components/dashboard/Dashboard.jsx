55ecba816fc8d4b723d65439c0a6d287baa2512a
Gajendra Singh <gajendrasp.rj.9693@gmail.com>
2017-06-06 17:46:40 +0530

    Added ongoing waves on dashboard

--- client/components/dashboard/Dashboard.jsx
+++ client/components/dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import FlatButton from 'material-ui/FlatButton';
 import {Grid, Row, Col} from 'react-flexbox-grid/lib';
 import Paper from 'material-ui/Paper';
 import Moment from 'moment';
+import WaveDetails from './WaveDetails.jsx';
 
 const styles = {
 	lastLogin: {
@@ -28,26 +29,12 @@ export default class Dashboard extends React.Component {
 		this.state = {
 			user: {}
 		}
-		this.getUser = this.getUser.bind(this);
 		this.formatDate = this.formatDate.bind(this); 
 	}
 	componentDidMount() {
-		this.getUser()
-	}
-	getUser() {
-		let th = this
-		Request
-			.get('/dashboard/user')
-			.set({'Authorization': localStorage.getItem('token')})
-			.end(function(err, res){
-				if(err)
-					console.log(err)
-				else {
-					th.setState({
-						user: res.body
-					})
-				}
-			})
+		this.setState({
+			user: this.props.user
+		})
 	}
 	formatDate(date) {
 		if(date) {
@@ -60,24 +47,17 @@ export default class Dashboard extends React.Component {
 	render() {
 		return(
 			<div>
-				<h1 style={styles.heading}>This is {this.state.user.name} dashboard</h1>
+				<h2 style={styles.heading}>Dashboard</h2>
 				<Grid>
 					<Row>
-						<Col md={6} mdOffset={1} >
-							<Card style={styles.card} >
-						    <CardHeader
-						      title={this.state.user.name}
-						      subtitle={this.state.user.username}
-						      avatar="../assets/images/avt-default.jpg"
-						    />
-						    <CardMedia
-						      overlay={<CardTitle title={this.state.user.name} subtitle={this.state.user.username} />}
-						    >
-						      <img src="../assets/images/avt-default.jpg" />
-						    </CardMedia>
-						  </Card>
-					  </Col>
-					  <Col md={3} mdOffset={1} >
+						<Col md={5} mdOffset={1}>
+							{
+								(this.state.user.role == 'wiproadmin' ||
+									this.state.user.role == 'sradmin') &&
+								<WaveDetails />
+							}
+						</Col>
+					  <Col md={3} mdOffset={2} >
 					  	<Paper style={styles.lastLogin} zDepth={1} >
 					  		<strong>Last Login: </strong> 
 					  		{this.formatDate(this.props.user.lastLogin)}
