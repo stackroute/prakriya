f8de0aef2c433105a97cff20ef34160251dbbed0
Gajendra Singh <gajendrasp.rj.9693@gmail.com>
2017-06-13 14:27:11 +0530

    Profile downloading for candidates

--- client/components/candidateManagement/CandidateCard.jsx
+++ client/components/candidateManagement/CandidateCard.jsx
@@ -4,8 +4,10 @@ import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
 import Dialog from 'material-ui/Dialog';
 import DeleteIcon from 'material-ui/svg-icons/action/delete';
+import DownloadIcon from 'material-ui/svg-icons/file/file-download';
 import {lightBlack} from 'material-ui/styles/colors';
 import Request from 'superagent';
+import jsPDF from 'jspdf';
 import dialog from '../../styles/dialog.json';
 
 const styles = {
@@ -30,6 +32,7 @@ export default class CandidateCard extends React.Component {
 		super(props);
 		this.state = {
 			showDeleteDialog: false,
+			showDownloadDialog: false,
 			imageURL: '../../assets/images/avt-default.jpg',
 		}
 		this.getProfilePic = this.getProfilePic.bind(this);
@@ -37,6 +40,7 @@ export default class CandidateCard extends React.Component {
 		this.handleCardClick = this.handleCardClick.bind(this);
 		this.openDeleteDialog = this.openDeleteDialog.bind(this);
 		this.closeDeleteDialog = this.closeDeleteDialog.bind(this);
+		this.downloadProfile = this.downloadProfile.bind(this);
 	}
 	componentWillMount() {
 		this.getProfilePic(this.props.candidate.EmployeeID);
@@ -46,17 +50,16 @@ export default class CandidateCard extends React.Component {
 		Request
 			.get(`/dashboard/getimage?eid=${eid}`)
 			.set({'Authorization': localStorage.getItem('token')})
-			.query({q: eid})
 			.end(function(err, res) {
 				if(err)
-		    	console.log(err);
+		    	console.log('Image not found for ', eid);
 		    else {
 		    	if(res.text) {
 		    		let array = new Uint8Array(res.text.length);
 		        for (var i = 0; i < res.text.length; i++){
 		            array[i] = res.text.charCodeAt(i);
 		        }
-		        var blob = new Blob([array], {type: 'image/jpeg'});
+		        let blob = new Blob([array], {type: 'image/jpeg'});
 			    	let blobUrl = URL.createObjectURL(blob);
 			    	th.setState({
 			    		imageURL: blobUrl
@@ -80,6 +83,19 @@ export default class CandidateCard extends React.Component {
 	}
 	handleDelete() {
 		this.props.handleDelete(this.props.candidate);
+	}
+	downloadProfile() {
+		let doc = new jsPDF()
+		let x = 10;
+		let y = 20;
+		doc.setFontSize(22);
+		doc.text(110, y+=10, this.props.candidate.EmployeeName, 'center')
+		doc.setFontSize(14);
+		doc.text(x, y+=10, this.props.candidate.EmployeeID+'')
+		doc.text(x, y+=10, this.props.candidate.EmailID)
+		// doc.addImage(this.state.imageURL, 'JPEG', x, y+=10)
+		doc.text(x, y+=10, $('<div>Hello</div>').html());
+		doc.save(this.props.candidate.EmployeeID + '.pdf')
 	}
 
 	render() {
@@ -117,6 +133,13 @@ export default class CandidateCard extends React.Component {
 			    	style={styles.cardTitle}
 			    />
 			    <CardActions style={styles.actions}>
+			    	<IconButton 
+			    		tooltip="Download Profile" 
+			    		style={{float: 'left'}}
+			    		onTouchTap={this.downloadProfile}
+			    	>
+				      <DownloadIcon color={lightBlack} />
+				    </IconButton>
 				    <IconButton tooltip="Delete Candidate" onTouchTap={this.openDeleteDialog}>
 				      <DeleteIcon color={lightBlack} />
 				    </IconButton>
