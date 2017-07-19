7a8dc645854cc9f07873b3a54b98d9a2520c2f24
RamvigneshPasupathy <ramvignesh06@gmail.com>
2017-07-19 16:24:10 +0530

    updated candidate feedback: minor ui changes

--- client/components/feedback/Feedback.jsx
+++ client/components/feedback/Feedback.jsx
@@ -278,14 +278,15 @@ export default class Feedback extends React.Component {
                         </Col>
                         <Col md={2}>
                           <StarRating
-                            color1={'#DDDDDD'} color2={'#00BCD4'}
+                            color1={'#DDDDDD'}
+                            color2={'#00BCD4'}
                             half={false} size={30} char={'\u263B'}
                             value={th.state[item.type][index]}
                             onChange={(newVal) => th.handleChange(newVal, item.type, index)}
                           />
                           {
-                            th.state.verify && th.state[item.type][index] === 0
-                            && <p>This is a required field</p>
+                            th.state.verify && th.state[item.type][index] == 0
+                            && <span style={{color: '#DD0000'}}>Please provide a rating</span>
                           }
                         </Col>
                       </Row>
@@ -297,11 +298,29 @@ export default class Feedback extends React.Component {
           }
           <Row><Col md={8} mdOffset={2}><h4>YOUR COMMENTS</h4></Col></Row>
           <Row><Col md={8} mdOffset={2}>
-              <TextField hintText="Express your views" floatingLabelText="Things you liked most about the program" multiLine={true} rows={3} rowsMax={3} fullWidth={true} value={this.state.mostLiked} onChange={this.handleMostLikedChange} disabled={th.state.oldFeedback}/>
+              <TextField
+                floatingLabelText={'Things you liked most about the program -- chars left: ' + (350 - th.state.mostLiked.length)}
+                multiLine={true}
+                rows={3}
+                rowsMax={3}
+                fullWidth={true}
+                value={this.state.mostLiked}
+                onChange={this.handleMostLikedChange}
+                disabled={th.state.oldFeedback}
+                maxLength="350" />
           </Col></Row>
 
           <Row><Col md={8} mdOffset={2}>
-              <TextField hintText="Express your views" floatingLabelText="Things you liked least about the program" multiLine={true} rows={3} rowsMax={3} fullWidth={true} value={this.state.leastLiked} onChange={this.handleLeastLikedChange} disabled={th.state.oldFeedback}/>
+              <TextField
+                floatingLabelText={'Things you liked least about the program -- chars left: ' + (350 - th.state.leastLiked.length)}
+                multiLine={true}
+                rows={3}
+                rowsMax={3}
+                fullWidth={true}
+                value={this.state.leastLiked}
+                onChange={this.handleLeastLikedChange}
+                disabled={th.state.oldFeedback}
+                maxLength="350" />
           </Col></Row>
 
           <Row><Col md={8} mdOffset={2} style={styles.submit}>
