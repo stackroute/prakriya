f5df73da5b6e4da7c1c3c0d3f9f2e01ee08172a1
Gajendra Singh <gajendrasp.rj.9693@gmail.com>
2017-07-20 10:44:35 +0530

    WAdmin dashboard

--- server/upload/uploadRouter.js
+++ server/upload/uploadRouter.js
@@ -84,9 +84,90 @@ router.post('/merge', auth.canAccess(CONFIG.ADMINISTRATOR), function (req, res) 
 	form.parse(req, function (err1, fields, files) {
 		fs.readFile(files.zcop.path, 'utf8', (err2, zcop_data) => {
 			fs.readFile(files.erd.path, 'utf8', (err2, erd_data) => {
-				logger.debug('zcop data', zcop_data)
-				logger.debug('erd data', erd_data)
-				
+				try {
+					let result = [];
+
+					let lines = zcop_data.split('\n');
+					let headers = lines[0].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+					lines.map(function (line, index) {
+						if(index > 0 && line !== '') {
+							let lineCol = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+							let cadetObj = {};
+
+							headers.map(function (head, key) {
+								if(lineCol[key] !== '') {
+									cadetObj[head] = lineCol[key];
+								}
+							});
+
+							let obj = {};
+
+							obj['Employee ID'] = cadetObj['EMP_CODE'];
+							obj['Employee Name'] = cadetObj['EMP_NAME'];
+							obj['Email ID'] = cadetObj['EMPLOYEE_EMAIL_ID'];
+							obj['Digi-thon Qualified'] = '';
+							obj['WT/WI'] = '';
+							obj['Billability'] = cadetObj['BILLABILITY_STATUS'];
+							obj['Career Band'] = cadetObj['CAREER_BAND'];
+							obj['Initial BU'] = ''
+							if(cadetObj.MODEL_TYPE == 'A' || cadetObj.MODEL_TYPE == 'C') {
+								obj['Revised BU'] = cadetObj.SAP_PRAC_DESC;
+							}
+							else if(cadetObj.MODEL_TYPE == 'B' || cadetObj.MODEL_TYPE == 'N' || cadetObj.MODEL_TYPE == 'R') {
+								obj['Revised BU'] = cadetObj.SAP_BU_DESC
+							}
+							obj['Work Experience'] = cadetObj['EXPERIENCE'];
+							obj['Digi-Thon Phase'] = '';
+							obj['Digi-thon Score'] = '';
+							obj['Training Status'] = '';
+							obj['Training Track'] = '';
+							obj['Wave'] = '';
+							obj['Start Date'] = '';
+							obj['End Date'] = '';
+							obj['Academy Training Skills'] = '';
+							obj['Skill - Set'] = '';
+							obj['Cadet Evaluation'] = '';
+							obj['Plan for Available Pool'] = '';
+							obj['Comments / Current / Future Plan'] = '';
+							obj['Project Status'] = '';
+							obj['Project - Billability'] = '';
+							obj['Remarks / Account Name'] = '';
+							obj['Project Name'] = '';
+							obj['Project Status'] = '';
+							obj['Project Status'] = '';
+							obj['sap'] = cadetObj['PROJECT'];
+							obj['Future Billability'] = '';
+							obj['Future BD'] = '';
+							obj['Project End date'] = '';
+							obj['Chk'] = '';
+							obj['Leadership Report'] = '';
+							obj['Resignation Reason'] = '';
+							obj['Pledge'] = '';
+							obj['Preferred Location 1'] = '';
+							obj['Preferred Location 2'] = '';
+							obj['Preferred Location 3'] = '';
+							obj['PRP Score'] = '';
+							obj['Home / Prac CC'] = cadetObj['PRAC_CC'];
+							obj['Cost Center'] = cadetObj['COSTCTR'];
+							obj['Primary Supervisor'] = cadetObj['PRIMARY_SUPERVISOR'];
+							obj['Project Supervisor'] = cadetObj['PROJECT_SUPERVISOR'];
+							obj['EM'] = '';
+							obj['Reported'] = '';
+							obj['Mentor Meet'] = '';
+							obj['Interns / FEP'] = '';
+							obj['FEP_ZCOP'] = '';
+							obj['FEP Tagging'] = '';
+							obj['FEP Reason'] = '';
+							result.push(obj);
+						}
+					});
+					res.status(200).json(result);
+				} catch(err4) {
+					logger.error('Error', err4);
+					res.status(500).json({
+						error: 'Internal error occurred, please report...!'
+					});
+				}
 			});
 		});
 	});
