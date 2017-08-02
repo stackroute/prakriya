c6dedfa8e50aa4f0f9b2460fc96be0d6644888e9
RamvigneshPasupathy <ramvignesh06@gmail.com>
2017-08-02 15:33:35 +0530

    refactored CourseSubCard

--- client/components/courseManagement/CourseSubCard.jsx
+++ client/components/courseManagement/CourseSubCard.jsx
@@ -77,7 +77,7 @@ export default class CourseCard extends React.Component {
       WeekErrorText: '',
       Duration: '',
       DurationErrorText: '',
-      searchPerm: '',
+      Skill: '',
       Day: '',
       DayErrorText: '',
       snackbarOpen: false,
@@ -94,8 +94,8 @@ export default class CourseCard extends React.Component {
     this.onChangeDuration = this.onChangeDuration.bind(this);
     this.onChangeWeek = this.onChangeWeek.bind(this);
     this.onChangeDay = this.onChangeDay.bind(this);
-    this.handleUpdateInputPerm = this.handleUpdateInputPerm.bind(this);
-    this.handleAddNewPerm = this.handleAddNewPerm.bind(this);
+    this.handleUpdateSkill = this.handleUpdateSkill.bind(this);
+    this.handleAddSkill = this.handleAddSkill.bind(this);
     this.handleSkillDelete = this.handleSkillDelete.bind(this);
     this.hideSnackbar = this.hideSnackbar.bind(this);
     this.openSnackbar = this.openSnackbar.bind(this);
@@ -238,36 +238,36 @@ export default class CourseCard extends React.Component {
     }
   }
 
-  handleUpdateInputPerm(searchPerm) {
+  handleUpdateSkill(Skill) {
 		this.setState({
-			searchPerm: searchPerm
+			Skill: Skill
 		})
 	}
 
-	handleAddNewPerm() {
+	handleAddSkill() {
     let th = this;
-		let perms = this.state.Skills
-    let perm = this.state.searchPerm;
-    let duplicateFound = perms.some(function(p) {
-      return p.toLowerCase() == perm.toLowerCase()
+		let skills = this.state.Skills
+    let skill = this.state.Skill;
+    let duplicateFound = skills.some(function(p) {
+      return p.toLowerCase() == skill.toLowerCase()
     });
     if(duplicateFound) {
       th.openSnackbar('Duplicate Skill! Try adding a new skill.');
     } else {
-      perms.push(perm)
+      skills.push(skill)
       th.setState({
-        Skills: perms,
-        searchPerm: '',
+        Skills: skills,
+        Skill: '',
         SkillErrorText: ''
       })
     }
 	}
 
-  handleSkillDelete(perm) {
-    let skill = this.state.Skills.filter(function(control) {
-      return perm != control
+  handleSkillDelete(skill) {
+    let skills = this.state.Skills.filter(function(s) {
+      return skill != s
     })
-    this.setState({Skills: skill})
+    this.setState({Skills: skills})
   }
 
   render() {
@@ -350,9 +350,9 @@ export default class CourseCard extends React.Component {
           <AutoComplete
             floatingLabelText="Add Skills"
             filter={AutoComplete.fuzzyFilter}
-            searchText={this.state.searchPerm}
-            onUpdateInput={this.handleUpdateInputPerm}
-            onNewRequest={this.handleAddNewPerm}
+            searchText={this.state.Skill}
+            onUpdateInput={this.handleUpdateSkill}
+            onNewRequest={this.handleAddSkill}
             dataSource={this.props.course.Skills}
             errorText={th.state.SkillErrorText}
             maxSearchResults={5}
