0219744d8cec747f9785c0f61bf25c0d063552a0
RamvigneshPasupathy <ramvignesh06@gmail.com>
2017-08-21 10:17:34 +0530

    refactored product profile

--- client/components/productProfile/ProductProfile.jsx
+++ client/components/productProfile/ProductProfile.jsx
@@ -1,50 +1,43 @@
 import React from 'react';
-import Paper from 'material-ui/Paper';
 import Request from 'superagent';
-
-const styles = {
-	paper: {
-		padding: '10px'
-	}
-};
 
 export default class ProductProfile extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			product: {},
-			versionIndex: 0
+			version: null
 		}
 
-		this.getProduct = this.getProduct.bind(this);
+		this.getProductVersion = this.getProductVersion.bind(this);
 	}
 
 	componentWillMount() {
-		this.getProduct();
+		this.getProductVersion();
 	}
 
-	getProduct() {
+	getProductVersion() {
 		let th = this;
 		Request
-			.get('/dashboard/project/' + th.props.routeParams.name)
+			.get('/dashboard/version/' + th.props.routeParams.name)
 			.set({'Authorization': localStorage.getItem('token')})
 			.end(function(err, res) {
-				if(err)
-		    	console.log(err);
-		    else {
-					console.log('Product: ', res.body)
-					th.setState(res.body);
+				if(err) {
+					console.log(err);
+				} else {
+					th.setState({version: res.body.version});
 		    }
 		  });
 	}
 
 	render() {
+		let th = this;
 		return (
 			<div>
-				<Paper style={styles.paper}>
-					<center>ProductName: {this.props.routeParams.name}</center>
-				</Paper>
+				<h3>
+					<center>{th.props.routeParams.name.toUpperCase()}</center>
+				</h3>
+				<br />
 			</div>
 		);
 	}
